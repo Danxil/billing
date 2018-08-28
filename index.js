bb2eb8c43aa8d796edb63714770f4f5d8dc00f68
Vadym Bondarenko <danxil@list.ru>
2018-08-29 00:12:30 +0300

    Added kassa proxy

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ import expressSslify from 'express-sslify';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import path from 'path';
+import url from 'url';
 import cors from 'cors';
 import routes from './routes';
+import proxy from 'express-http-proxy';
 
 require('dotenv').config();
 
@@ -15,6 +17,10 @@ app.use(cors({
 }));
 app.use(cookieParser());
 app.use(bodyParser.json({ extended: true }));
+app.use('/free-kassa', proxy('fun-spin.com', {
+  https: true,
+  proxyReqPathResolver: (req) => `/api/free-kassa${url.parse(req.url).path}`,
+}));
 
 app.use(express.static(path.join(__dirname, 'client'), { index: false, extensions: false, redirect: false }));
 
