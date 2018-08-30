3356b4a174e1642cc663d47233fce703251e5e61
Vadym Bondarenko <danxil@list.ru>
2018-08-30 23:21:13 +0300

    Added kassa proxy

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 import path from 'path';
 
 export default ({ app }) => {
-  app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../', 'client', 'index.html'));
-  });
   app.get('/free-kassa/success', (req, res) => {
     res.redirect('https://fun-spin.com/by-coins');
+  });
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../', 'client', 'index.html'));
   });
 };
