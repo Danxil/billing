431387e47df60b2f5c5a0a1ed548915ba5a4a19c
Vadym Bondarenko <danxil@list.ru>
2019-02-16 23:40:35 +0200

    Added hp-adv-cash

---
+++ client/precache-manifest.4c12bd0d286c46ec32607cf4093f49d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "402b1e2d2b3810314dcf",
+    "url": "/static/js/main.402b1e2d.chunk.js"
+  },
+  {
+    "revision": "2b39c5efcac001a0d27f",
+    "url": "/static/js/2.2b39c5ef.chunk.js"
+  },
+  {
+    "revision": "402b1e2d2b3810314dcf",
+    "url": "/static/css/main.9532c935.chunk.css"
+  },
+  {
+    "revision": "2b39c5efcac001a0d27f",
+    "url": "/static/css/2.b55b2403.chunk.css"
+  },
+  {
+    "revision": "65ff2aab0e9425684396a3ffd9f556bd",
+    "url": "/index.html"
+  }
+];
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ export default ({ app }) => {
   app.get('/free-kassa/success', (req, res) => {
     res.redirect('https://fun-spin.com/by-coins');
   });
+  app.get('/hp/adv-cash/success/', (req, res) => {
+    res.redirect('https://fast-credit.in');
+  });
   app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../', 'client', 'index.html'));
   });
